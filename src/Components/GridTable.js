ee4b9b16fcd7d3e29ba049efd488e08d062eda25
LSilva <lucas@shifta.la>
2023-01-05 13:52:03 -0300

    Funcionalidad de Filtro y Carga de tabla

--- src/Components/GridTable.js
+++ src/Components/GridTable.js
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "../Styles/GridTable.module.css";
 
-export const GridTable = () => {
+export const GridTable = (props) => {
     return (
-        <div className={styles["table-container"]}></div>
+        <div className={styles["table-container"]}>
+            <table>
+                <thead>
+                    <tr>
+                    {props.columns.map((item) => 
+                        <th>{item}</th>
+                    )}
+                    </tr>
+                </thead>
+                <tbody>
+                        {
+                        props.rows.map((item) =>
+                        <tr>
+                            <td>{item.Id}</td>
+                            <td>${item.Precio}</td>
+                            <td>{item.FechaCarga}</td>
+                            <td>{item.Categoria}</td>
+                        </tr>
+                        )}
+                </tbody>
+            </table>
+        </div>
     );
 }
--- src/Components/StockManagement.js
+++ src/Components/StockManagement.js
@@ -3,26 +3,78 @@ import styles from "../Styles/StockManagement.module.css";
 import { GridTable } from "./GridTable";
 
 const StockManagement = () => {
+    const columns = ["Id", "Precio", "Fecha de Carga", "Categoría"];
+    const [rows, setRows] = useState([]);
     const [productos, setProductos] = useState([]);
+    const [presupuesto, setPresupuesto] = useState(null);
+    const [resultado, setResultado] = useState({montoTotal: null, productos:[]});
     
     function GetProductos() {
-        setProductos([]);
+        var data = [
+            {Id: 1, Nombre:"Prod 1", Precio: 5, FechaCarga:"26/10/2019", Categoria:"Produno"},
+            {Id: 2, Nombre:"Prod 2", Precio: 10, FechaCarga:"25/10/2019", Categoria:"Proddos"},
+            {Id: 3, Nombre:"Prod 3", Precio: 15, FechaCarga:"26/10/2019", Categoria:"Produno"},
+            {Id: 4, Nombre:"Prod 4", Precio: 20, FechaCarga:"27/10/2019", Categoria:"Produno"},
+            {Id: 5, Nombre:"Prod 5", Precio: 25, FechaCarga:"28/10/2019", Categoria:"Proddos"},
+        ];
+        setProductos(data);
+        setRows(data);
+    }
+
+    function GetProductoPrecioMayor(prods){
+        var precio = 0;
+        var prod = null;
+        prods.forEach(function(value){
+            if(value > precio){
+                precio = value;
+            }
+            else {
+                prod = value;
+            }
+        });
+
+        return prod;
     }
     
     function GetProductosByMontoCliente(e){
-        
+        var presupuesto = e.target.value;
+        var productosFiltrados = productos.filter(x => x.Precio < presupuesto);
+        var productosProddos = productosFiltrados.filter(x => x.Categoria === "Proddos");
+        var productosProduno = productosFiltrados.filter(x => x.Categoria === "Produno");
+        var prodMayorPrecio = GetProductoPrecioMayor(productosProddos);
+
+        productosProduno.forEach(function(value){
+            var suma = (prodMayorPrecio.Precio + value.Precio);
+            if(suma <= presupuesto){
+                setResultado({montoTotal: suma, productos: [value.Nombre, prodMayorPrecio.Nombre]});
+            }
+        });  
+
+        setPresupuesto(e.target.value);
+        setRows(productosFiltrados);
     }
 
     useEffect(()=>{
-
+        GetProductos();
     }, []);
 
     return (
         <div className={styles.container}>
             <div className={styles["filter-container"]}>
-                <input type="text" className={styles.filter} onChangeCapture={(e)=> GetProductosByMontoCliente(e)}/>
+                <input type="text" className={styles.filter} onChange={(e)=> GetProductosByMontoCliente(e)}/>
             </div>
-            <GridTable />
+            <GridTable columns={columns} rows={rows}/>
+            {
+                <div className={styles["result-container"]}>
+                    <p>Presupuesto: {rows.length > 0 ? presupuesto : null}</p>
+                    <p>Productos Recomendados: 
+                        {rows.length > 0 
+                        ? resultado.productos.map((item) => <li>{item}</li>) 
+                        : null}
+                    </p>
+                    <p>Monto Total: {rows.length > 0 ? resultado.montoTotal : null}</p>
+                </div>
+            }
         </div>
     );
 };
