019b9f938f69b4bac343425f01bbc0279dfad5e6
LSilva <lucas@shifta.la>
2023-01-05 02:27:04 -0300

    Creacion de componentes y estilos

---
+++ src/Components/GridTable.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../Styles/GridTable.module.css";
+
+export const GridTable = () => {
+    return (
+        <div className={styles["table-container"]}></div>
+    );
+}
